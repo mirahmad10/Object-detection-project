12ca71698d4e957a59f7ba0183c0c9335f0b682a
Akif Ahmad <120121792+Top-B@users.noreply.github.com>
2025-01-03 20:42:04 +0600

    Update object_detection.py after suzz

--- object_detection.py
+++ object_detection.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, render_template, request, jsonify
 import cv2
 from ultralytics import YOLO  
 import threading
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,45 +11,57 @@ classes = model.names
 webcam_active = False
 cap = None
 event = threading.Event()
+counter = 1
+results = []
+
+def error_frame(error):
+    img = np.zeros((480, 640, 3), dtype=np.uint8)
+    cv2.putText(img, str(error), (10, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    _, img = cv2.imencode('.jpg', img)
+    img = img.tobytes()
+    return(b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
+
 
 def generate_frames():
+    global counter, results
     while event.is_set():
         try:
             ret, img= cap.read()
             if not ret:
-                return "Error: Failed to capture frame."
+                yield error_frame('Error: Unable to read camera')
         except Exception as err:
-            return 'Error: Could not read from camera'
-        
+            yield error_frame(err.__str__())
+            continue
         try:
-            results = model.predict(img, stream=True, verbose=False)
+            if counter%7==0:
+                results.clear()
+                results.append(list(model.predict(img, stream=True, verbose=False)))
         except Exception as err:
-            return 'Error: Model could not predict'
-        
-        try:
-            for x in results:
-                for y in x.boxes: 
-                    if y.conf > 0.6:
-                        x1, y1, x2, y2 = y.xyxy[0]
-                        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
-                        text_loc = [x1, y1]
+            yield error_frame(err.__str__())
+            continue
 
-                        predicted_class = str(classes[int(y.cls)]).upper()
-                        confidence = str(int(y.conf*100))
-                        text = f"{predicted_class} [{confidence}%]"
-                        font = cv2.FONT_HERSHEY_SIMPLEX
-                        fontsize = 0.7
-                        color = (255, 0, 0)
-                        thickness = 2
-                        cv2.putText(img, text, text_loc, font, fontsize, color, thickness)
-                    else:
-                        continue
+        try:
+            if results:
+                for x in results[0]:
+                    for y in x.boxes: 
+                        if y.conf > 0.6:
+                            x1, y1, x2, y2 = y.xyxy[0]
+                            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
+                            text_loc = (x1, y1)
+                            predicted_class = str(classes[int(y.cls)]).upper()
+                            confidence = str(int(y.conf*100))
+                            text = f"{predicted_class} [{confidence}%]"
+                            cv2.putText(img, text, text_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
         except Exception as err:
-            return 'Error: Could not draw on image'
+            yield error_frame(err.__str__())
+            continue
         
         _, img = cv2.imencode('.jpg', img)
         img = img.tobytes()
+        counter+=1
         yield(b'--frame\r\n'
               b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
 
@@ -64,22 +77,21 @@ def toggle_webcam():
             data = request.get_json()
             action = data.get('action')
         except Exception as err:
-            return jsonify({'status': 'error', 'message': err})
+            return jsonify({'status': 'error', 'message': err.__str__()})
 
         if action == 'start':
             if not webcam_active:
-                
                 try:
                     cap = cv2.VideoCapture(0)
                     if not cap.isOpened():
-                        return jsonify({"status": "error", "message": "Unable to access webcam"}), 500
+                        return jsonify({"status": "error", "message": "Unable to access webcam"})
                     cap.set(3, 640)
                     cap.set(4, 480)
                     webcam_active = True
                     event.set()
                     return jsonify({"status": "success", "webcam_active": webcam_active})
                 except Exception as err:
-                    return jsonify({"status": "error", "message": err})    
+                    return jsonify({"status": "error", "message": err.__str__()})    
             
             elif webcam_active:
                 return jsonify({"status": "error", "message": 'Webcam already active.'})
@@ -94,13 +106,13 @@ def toggle_webcam():
                     cv2.destroyAllWindows()
                     return jsonify({"status": "success", "webcam_active": webcam_active})
                 except Exception as err:
-                    return jsonify({"status": "error", "message": err})   
+                    return jsonify({"status": "error", "message": err.__str__()})   
                 
             elif not webcam_active:
-                return jsonify({"status": "success", "message": "Webcam already stopped"})
+                return jsonify({"status": "error", "message": "Webcam already stopped"})
         
         else:
-            return jsonify({"status": "error", "message": "Invalid action"}), 400
+            return jsonify({"status": "error", "message": "Invalid action"})
 
     elif request.method == 'GET':
         return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame;')
